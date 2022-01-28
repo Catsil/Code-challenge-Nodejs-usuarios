487692fde8641ed1d5a3723158c9731e5e463e37
unknown <imgloriasilva@gmail.com>
2022-01-28 16:29:36 +0100

    server config

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
 const express = require('express');
 const morgan=require('morgan');
+const exphbs= require('express-handlebars');
+const path= require('path');
 
 
 const app= express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views')); 
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers:require('./lib/handlebars')
+}));
 
+app.set('view engine', '.hbs');
 
+//middlewares
 app.use(morgan('dev'));
+app.use(express.json());
 
+//global variables
+app.use((req, res , next)=>{
+    next();
+});
+
+//routes
 app.use(require('./routes/'));
+app.use(require('./routes/autentication'));
+app.use('/links', require('./routes/links'));
 
+//public
+app.use(express.static(path.join(__dirname, 'public')));
+
+//starting th server
 app.listen(app.get('port'), ()=>{
     console.log('server on port', app.get('port'));
 });
